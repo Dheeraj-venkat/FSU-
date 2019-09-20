88366c51858964c85ef3495ee2231c05a0be0b6d
Dheeraj Katta <dheeraj.venkat90@gmail.com>
2019-09-20 16:07:06 -0400

    Demo Project
    Demo project

---
+++ Demo Project/resources/js/dheeraj.js
@@ -0,0 +1,4 @@
+function spectrum(){
+ var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+ $('.blinking').css({color:hue});
+}
